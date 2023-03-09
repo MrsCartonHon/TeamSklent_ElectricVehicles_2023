86cb26ba9c0e40ede8adff714faff8bee774feb6
AllaNirmal <allanirmal@pleasval.org>
2023-03-09 15:02:21 -0600

    sdk

--- app/src/main/java/com/example/teamsklent_electricvehicles_2023/MainActivity.kt
+++ app/src/main/java/com/example/teamsklent_electricvehicles_2023/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.teamsklent_electricvehicles_2023
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.ui.unit.sp
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.teamsklent_electricvehicles_2023.ui.theme.TeamSklent_ElectricVehicles_2023Theme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +31,27 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.deere_logo),
+                            contentDescription = stringResource(id = R.string.deere_logo),
+                            modifier = Modifier.padding(bottom = 100.dp)
+                        )
+                        Text(
+                            text = "Create Your Account",
+                            style = TextStyle(
+                                fontSize = 24.sp
+                            ),
+                            modifier = Modifier.padding(vertical = 10.dp),
+                            textAlign = TextAlign.Center
+                        )
+                        InputBox(label = "Enter Your Name")
+                        InputBox(label = "Enter Your Password")
+                        CreateAccount()
+                    }
                 }
             }
         }
@@ -34,10 +63,31 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InputBox(label: String){
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    OutlinedTextField(
+        value = text,
+        label = { Text(text = "$label") },
+        onValueChange = {
+            text = it
+        },
+        modifier = Modifier.padding(horizontal = 100.dp)
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     TeamSklent_ElectricVehicles_2023Theme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun CreateAccount(){
+    Button(onClick = {}, modifier = Modifier.padding(horizontal = 100.dp, vertical = 50.dp)) {
+        Text(text = "Create")
     }
 }
