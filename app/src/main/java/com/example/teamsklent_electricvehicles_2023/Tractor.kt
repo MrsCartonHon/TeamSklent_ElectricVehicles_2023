4639c44368d5bcbb8c01cc6ef7ecf36f2d2e111f
Dominic Halbur <67172504+DominicHalbur@users.noreply.github.com>
2023-03-10 13:55:02 -0600

    added FAB to home

--- app/src/main/java/com/example/teamsklent_electricvehicles_2023/Tractor.kt
+++ app/src/main/java/com/example/teamsklent_electricvehicles_2023/Tractor.kt
@@ -1,5 +1,7 @@
 package com.example.teamsklent_electricvehicles_2023
 
 sealed class Tractor(var name: String, var icon: Int, var AHCapacity: Double){
-    object x250 : Tractor("X-250EV", 2, 250.0)
+    object X250 : Tractor("X-250EV", 2, 250.0)
+    object X300 : Tractor("X-300EV", 2, 300.0)
+    object X350 : Tractor("X-350", 2, 350.0)
 }
--- app/src/main/java/com/example/teamsklent_electricvehicles_2023/screens/Home.kt
+++ app/src/main/java/com/example/teamsklent_electricvehicles_2023/screens/Home.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.*
@@ -14,6 +15,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.teamsklent_electricvehicles_2023.ui.theme.JDGreen1
+import com.example.teamsklent_electricvehicles_2023.ui.theme.JDYellow
 
 @OptIn(androidx.compose.material3.ExperimentalMaterial3Api::class)
 @Composable
@@ -26,24 +29,17 @@ fun Home() {
             imageVector = Icons.Filled.Home,
             contentDescription = "Home",
             tint = Color.Blue,
-            modifier = Modifier.size(150.dp)
+            modifier = Modifier
+                .size(150.dp)
                 .align(Alignment.Center)
         )
     }
-    Column {
-        //List user's tractors here
-        ListItem(
-            headlineContent = { Text("Two line list item with trailing") },
-            supportingContent = { Text("Secondary text") },
-            trailingContent = { Text("meta") },
-            leadingContent = {
-                Icon(
-                    Icons.Filled.Favorite,
-                    contentDescription = "Localized description",
-                )
-            }
-        )
-        Divider()
-    }
-
+    FloatingActionButton(
+        onClick = { /*TODO*/ },
+        containerColor = JDGreen1,
+        contentColor = JDYellow,
+        content = {
+            Icon(imageVector = Icons.Filled.Add, contentDescription = "drive", tint = JDYellow)
+        }
+    )
 }
